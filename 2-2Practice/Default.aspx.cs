b9272067dcf0a6e4014ee1bfeb39821371d6dfd5
錦皇 吳 <107111238@mail.oit.edu.tw>
2021-03-16 17:25:24 +0800

    Leap year

--- 2-2Practice/Default.aspx.cs
+++ 2-2Practice/Default.aspx.cs
@@ -8,7 +8,21 @@ using System.Web.UI.WebControls;
 namespace _2_2Practice {
     public partial class Default : System.Web.UI.Page {
         protected void Page_Load(object sender, EventArgs e) {
-
+            int year = 8;
+            string result = "Y";
+            if (year % 4 == 0)
+            {
+                if (year % 100 == 0)
+                {
+                    if (year % 400 != 0)
+                        result = "N";
+                }
+            }
+            else
+            {
+                result = "N";
+            }
+            Response.Write(result);
         }
     }
 }
